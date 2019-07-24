fff53a0e99f85bce67ce93fe8a77b65b2804cd3b
vijaykhokhar <vijaykhokhar312@gmail.com>
2019-07-24 18:17:54 +0530

    Meta's program

---
+++ MetaTest.rb
@@ -0,0 +1,16 @@
+animal = "dog"
+def animal.speak
+	puts "#{self} says WOOF"
+end
+
+singleton = class << animal
+	def lie
+		puts "#{self} lies down"
+	end
+self
+end
+
+#animal.speak
+#animal.lie
+#puts "Singleton class objects is #{singleton}"
+#puts "its defines method #{singleton.instance_methods - 'cat'.methods}"
